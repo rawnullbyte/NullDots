5d543aba574984d24b6012071af43a204c7527c6
rawnullbyte <andrexyt2012@gmail.com>
2025-11-19 17:44:40 +0100

    Update installer.py

--- installer.py
+++ installer.py
@@ -47,7 +47,7 @@ run("""sudo pacman-key --lsign-key 3056513887B78AEB""")
 run("""sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm""")
 run("""sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm""")
 run("""sudo grep -q "\\[chaotic-aur\\]" /etc/pacman.conf || \\
-    echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf\"""")
+    echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf""")
 run("""sudo pacman -Syu --noconfirm""")
 
 # Install yay
