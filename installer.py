9274742db796dc2a85113f3e5f64384149592865
rawnullbyte <andrexyt2012@gmail.com>
2025-11-19 17:52:39 +0100

    .

--- installer.py
+++ installer.py
@@ -89,9 +89,19 @@ for dotfile in dotfiles_data:
     logger.info(f"Post-copy commands: {post_copy}")
     logger.info("----------")
 
-    if pre_copy: run(pre_copy)
-    run(f"""sudo cp -R ./dotfiles/{source} {target}""")
-    if post_copy: run(post_copy)
+    if pre_copy:
+        r = run(pre_copy)
+        if r != 0:
+            logger.error(f"Pre-copy command failed: {pre_copy}")
+
+    r = run(f"cp -R {source} {target}")
+    if r != 0:
+        logger.error(f"Failed to copy {source} to {target}")
+
+    if post_copy:
+        r = run(post_copy)
+        if r != 0:
+            logger.error(f"Post-copy command failed: {post_copy}")
 
 # Enable ly
 run("sudo systemctl enable ly.service")
